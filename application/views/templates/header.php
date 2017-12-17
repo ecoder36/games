13cfeb0848da2f8b648ebd1f141ecab98aaba31f
samer <mrsamer123@hotmail.com>
2017-12-17 16:50:43 +0000

    Added iframe styles to a single view page

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -72,7 +72,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">IOGames</a>
+              <a class="navbar-brand" href="<?php echo base_url(); ?>admin">IOGames</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
               <ul class="nav navbar-nav">
